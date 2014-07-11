a68a30e1260981025f11c0ac2d474d7f91358785
Andreas Hagesjö <andreas.hagesjo@gmail.com>
2014-07-11 15:07:18 +0200

    Added background, more projectiles etc

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
 import time
 from pygame.locals import *
-import pygame, math, sys
+import pygame, math, sys, random
 
 WIDTH = 1920
 HEIGHT = 1200
 BLACK = (0,0,0)
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
+background = pygame.image.load('wallp.jpg')
 clock = pygame.time.Clock()
-caves = pygame.sprite.RenderPlain()
-ducks = pygame.sprite.RenderPlain()
+caves = pygame.sprite.Group()
+ducks = pygame.sprite.Group()
 
 class Duck(pygame.sprite.Sprite):
+	images = []
+	images.append(pygame.image.load('raju1.png'))
+	images.append(pygame.image.load('a1.png'))
+	speed = 30
 	def __init__(self, position, direction):
+
 		pygame.sprite.Sprite.__init__(self)
-		self.speed = 50
 		self.direction = direction
-		self.src_image = pygame.image.load('ammo.png')
+		self.src_image = self.images[random.randint(0,1)]
 		self.position = position
 		self.k_left = self.k_right = 0
 		
 	def update(self, deltat):
+
 		x,y = self.position
 		rad = self.direction * math.pi/180
 		x += -self.speed * math.sin(rad)
 		y += -self.speed * math.cos(rad)
+		#if (x >= width or y > height):
+
 		self.position = (x,y)
 		self.rect = self.src_image.get_rect()
 		self.rect.center = self.position
@@ -35,7 +43,7 @@ class Cave(pygame.sprite.Sprite):
 	MAX_FORWARD_SPEED = 10
 	MAX_REVERSE_SPEED = 10
 	ACCELERATION = 2
-	TURN_SPEED = 5
+	TURN_SPEED = 15
 
 	def __init__(self,image, position):
 		pygame.sprite.Sprite.__init__(self)
@@ -46,7 +54,7 @@ class Cave(pygame.sprite.Sprite):
 		self.images.append(pygame.image.load('c2.png'))
 		self.images.append(pygame.image.load('c3.png'))
 		self.images.append(pygame.image.load('c2.png'))
-		self.image = self.images[self.index]
+		self.image = self.images[0]
 
 		self.src_image = pygame.image.load(image)
 		self.position = position
@@ -80,31 +88,31 @@ caves.add(cave1)
 caves.add(cave2)
 
 while 1:
-	clock.tick(30)
+	clock.tick(60)
 	for event in pygame.event.get():
 		if not hasattr(event,'key'): continue
 		down = event.type == KEYDOWN
-		if event.key == K_RIGHT: 
-			cave1.k_right = down * -5
-		elif event.key == K_b: 
+		if event.key == K_b: 
 			cave1.ducklol()
+		elif event.key == K_RIGHT: 
+			cave1.k_right = down * -cave1.TURN_SPEED
 		elif event.key == K_LEFT:
-			cave1.k_left = down * 5
+			cave1.k_left = down * cave1.TURN_SPEED
 		elif event.key == K_UP:
 			cave1.k_up = down * 2
 		elif event.key == K_DOWN:
 			cave1.k_down = down * 2
 		elif event.key == K_i: 
-			cave2.k_right = down * -5
+			cave2.k_right = down * -cave2.TURN_SPEED
 		elif event.key == K_e:
-			cave2.k_left = down * 5
+			cave2.k_left = down * cave2.TURN_SPEED
 		elif event.key == K_p:
 			cave2.k_up = down * 2
 		elif event.key == K_u:
 			cave2.k_down = down * 2
 		elif event.key == K_ESCAPE: sys.exit(0)	 # quit the game
 	screen.fill(BLACK)
-
+	screen.blit(background, [0,0])
 	caves.update(30)
 	caves.draw(screen)
 	ducks.update(30)
