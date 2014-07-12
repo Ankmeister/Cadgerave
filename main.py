73776782ca9a0f6cac1a7b71e5791933cf361afa
--global <--global>
2014-07-12 19:05:07 +0200

    settinngs

--- main.py
+++ main.py
@@ -207,7 +207,7 @@ class Cave(pygame.sprite.Sprite):
 	
 def main():
 	pygame.mixer.music.play(-1)
-	for t in range(20,0,-1):
+	for t in range(100,0,-1):
 		screen.blit(startbackground, [0,0])
 		introPlayerOne = []
 		introPlayerTwo = []
